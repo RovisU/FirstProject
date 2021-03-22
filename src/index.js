31ad95a05044f35afcfefaee6d5f3686879d02c2
GuzMac <guzmac@GuzMacs-iMac.local>
2021-03-21 19:47:24 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// setting port
+app.set('port', 3000);
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+
+// midlewares
+
+// roures
+app.use(require('./routes/'));
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+// listening the server
+app.listen(app.get('port'), () => {
+    console.log('Listen on port', app.get('port'));
+});
