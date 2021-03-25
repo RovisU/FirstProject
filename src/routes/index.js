6f70f026bc611e8e72f8627c74cdec8b658020e7
GuzMac <guzmac@GuzMacs-iMac.local>
2021-03-25 13:22:47 -0500

    fogawebsite v1

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,4 +9,24 @@ router.get("/contact", (req, res) => {
   res.render("contact.html");
 });
 
+router.get("/telecomunicaciones", (req, res) => {
+  res.render("telecomunicaciones.html");
+});
+
+router.get("/catering", (req, res) => {
+  res.render("catering.html");
+});
+
+router.get("/licitaciones", (req, res) => {
+  res.render("licitaciones.html");
+});
+
+router.get("/confeccion", (req, res) => {
+  res.render("confeccion.html");
+});
+
+router.get("/distribucion", (req, res) => {
+  res.render("distribucion.html");
+});
+
 module.exports = router;
