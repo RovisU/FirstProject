39e6ed882713ea179eea8835f1634afa7b99f2c9
GuzMac <guzmac@GuzMacs-iMac.local>
2021-03-30 04:49:09 -0500

    contact form

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const nodemailer = require('nodemailer');
 
 // setting port
 app.set('port', 3000);
@@ -9,9 +10,39 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 
 // midlewares
+app.use(express.json());
 
-// roures
+// routes
 app.use(require('./routes/'));
+
+//nodemailer
+app.post('/', (req, res) => {
+    console.log(req.body)
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'grupofoga@gmail.com',
+            pass: 'rdvhimumnmjyhptc'
+        }
+    });
+
+    const mailOptions = {
+        from: req.body.email,
+        to: 'gerencia@inversionesfoga.com',
+        subject: `Mensaje de: ${req.body.name} Email: ${req.body.email} Asunto: ${req.body.subject}`,
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.send('error');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send('success')
+        }
+    })
+})
 
 //static files
 app.use(express.static(path.join(__dirname, 'public')))
---
+++ src/public/js/app.js
@@ -0,0 +1,96 @@
+function validateForm() {
+          var n = document.getElementById('name').value;
+          var e = document.getElementById('email').value;
+          var s = document.getElementById('subject').value;
+          var m = document.getElementById('message').value;
+          var onlyLetters = /^[a-zA-Z\s]*$/;
+          var onlyEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+
+          if (n == "" || n == null) {
+            document.getElementById('nameLabel').innerHTML = ('Ingresa tu nombre por favor');
+            document.getElementById('name').style.borderColor = "red";
+            return false;
+          }
+
+
+          if (!n.match(onlyLetters)) {
+            document.getElementById('nameLabel').innerHTML = ('Ingresa solo letras por favor');
+            document.getElementById('name').style.borderColor = "red";
+            return false;
+          }
+
+          if (e == "" || e == null) {
+            document.getElementById('emailLabel').innerHTML = ('Ingresa tu email por favor');
+            document.getElementById('email').style.borderColor = "red";
+            return false;
+          }
+
+          if (!e.match(onlyEmail)) {
+            document.getElementById('emailLabel').innerHTML = ('Ingresa una cuenta de email valida por favor');
+            document.getElementById('email').style.borderColor = "red";
+            return false;
+          }
+
+          if (s == "" || s == null) {
+            document.getElementById('subjectLabel').innerHTML = ('Por favor, ingresa el asunto');
+            document.getElementById('subject').style.borderColor = "red";
+            return false;
+          }
+
+          if (!s.match(onlyLetters)) {
+            document.getElementById('subjectLabel').innerHTML = ('Por favor, ingresa solo letras');
+            document.getElementById('subject').style.borderColor = "red";
+            return false;
+          }
+
+          if (m == "" || m == null) {
+            document.getElementById('messageLabel').innerHTML = ('Por favor, ingresa un mensaje');
+            document.getElementById('message').style.borderColor = "red";
+            return false;
+          }
+
+          else {
+            return true;
+          }
+
+        }
+
+
+const contactForm = document.querySelector('.form');
+let name = document.getElementById('name');
+let email = document.getElementById('email');
+let subject = document.getElementById('subject');
+let message = document.getElementById('message');
+
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('Mensaje enviado');
+
+    let formData = {
+        name: name.value,
+        email: email.value,
+        subject: subject.value,
+        message: message.value
+    }
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    if (validateForm() === true) {
+            xhr.onload = function () {
+            console.log(xhr.responseText);
+            if (xhr.responseText == 'success') {
+                alert('Mensaje enviado!');
+                name.value = '';
+                email.value = '';
+                subject.value = '';
+                message.value = '';
+            } else {
+                alert('Oops!, algo salio mal...')
+            }
+        }
+    }
+    xhr.send(JSON.stringify(formData));
+});
+
